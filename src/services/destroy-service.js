38409ac93dd4d9749545325aadcb54a0b5cffa00
sedtavud22 <phoosed@gmail.com>
2024-02-14 18:48:36 +0700

    destroyMany

--- src/services/destroy-service.js
+++ src/services/destroy-service.js
@@ -1,8 +1,18 @@
 const cloudinary = require("../config/cloudinary");
 
-exports.destroy = async (prefix) => {
+exports.destroy = async (publicKey) => {
   try {
-    const res = await cloudinary.api.delete_resources_by_prefix(prefix);
+    const res = await cloudinary.uploader.destroy(publicKey);
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log("Destroy error", error);
+  }
+};
+
+exports.destroyMany = async (publicKeyArray) => {
+  try {
+    const res = await cloudinary.api.delete_resources(publicKeyArray);
     console.log(res);
     return res;
   } catch (error) {
